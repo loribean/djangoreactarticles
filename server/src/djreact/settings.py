0c207944162bf812d6069fbb40d047783edc9cd6
loribean <68610463+loribean@users.noreply.github.com>
2020-10-02 10:44:30 +0800

    Update settings.py

--- server/src/djreact/settings.py
+++ server/src/djreact/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#!!-#t^ch8b#4gb7hw-=6#tntzml2mgrs_q^51ok!b^rt#@ce&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
